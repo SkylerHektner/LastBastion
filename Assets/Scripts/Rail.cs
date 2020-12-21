1a12270429f86070b5b29ceb3ce593694a2df052
Skyler Hektner <skylerhektner@gmail.com>
2020-12-20 18:15:00 -0800

    Add in basic rail and sawblade

---
+++ Assets/Scripts/Rail.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class Rail : MonoBehaviour
+{
+    [SerializeField] [Range(0.01f, 0.5f)] float vfxUpdateInterval = 0.1f;
+    [SerializeField] [Range( 0.01f, 1.0f )] float vfxLightningDensity = 0.1f;
+    [SerializeField] [Range( 0.01f, 1.0f )] float vfxLightningRange = 0.1f;
+    [SerializeField] GameObject topGameObject;
+    [SerializeField] GameObject bottomGameObject;
+    LineRenderer lineRenderer;
+
+    public float Top { get { return topGameObject.transform.position.y; } }
+    public float Bottom { get { return bottomGameObject.transform.position.y; } }
+    public float RailCenter { get { return transform.position.x; } }
+
+    public void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        StartCoroutine( updateLine() );
+    }
+
+    IEnumerator updateLine()
+    {
+        while( true )
+        {
+            updateLineRenderer();
+            yield return new WaitForSeconds( vfxUpdateInterval );
+        }
+    }
+
+    private void updateLineRenderer()
+    {
+        var points = addDeviance( topGameObject.transform.position, bottomGameObject.transform.position );
+        lineRenderer.positionCount = points.Length;
+        lineRenderer.SetPositions( points );
+    }
+
+    // creates a list of points with the appropriate deviance. Includes the start and end points
+    private UnityEngine.Vector3[] addDeviance( Vector3 start, Vector3 end )
+    {
+        List<Vector3> result = new List<Vector3>();
+        result.Add( start );
+        Vector3 path = end - start;
+        for( int i = 0; i * vfxLightningDensity < path.magnitude; i++ )
+        {
+            Vector3 point = new Vector3();
+            point.x = path.normalized.x * i * vfxLightningDensity + UnityEngine.Random.Range( -vfxLightningRange, vfxLightningRange );
+            point.y = path.normalized.y * i * vfxLightningDensity + UnityEngine.Random.Range( -vfxLightningRange, vfxLightningRange );
+            point.x += start.x;
+            point.y += start.y;
+            point.z = -1.0f;
+            result.Add( point );
+        }
+        result.Add( end );
+
+        return result.ToArray();
+    }
+}
---
+++ Assets/Scripts/Saw.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saw : MonoBehaviour
+{
+    [SerializeField] float MoveSpeed = 1.0f;
+    [SerializeField] Rail RightRail;
+    [SerializeField] Rail LeftRail;
+
+    private bool onLeftSide = true;
+    private Vector3 target_position;
+    bool moving = false;
+
+    private void Start()
+    {
+        target_position = transform.position;
+    }
+
+    private void Update()
+    {
+        if( moving )
+        {
+            if((transform.position - target_position).magnitude < MoveSpeed * Time.deltaTime)
+            {
+                transform.position = target_position;
+                moving = false;
+            }
+            else
+                transform.position = transform.position + ( target_position - transform.position ).normalized * MoveSpeed * Time.deltaTime;
+        }
+    }
+
+    public void GoToSide()
+    {
+        Vector3 touch_postition = Vector3.zero;
+#if PC
+        touch_postition = Input.mousePosition;
+#endif
+#if MOBILE
+        touch_postition = Input.GetTouch( 0 ).position;
+#endif
+
+        Vector3 world_pos = Camera.main.ScreenToWorldPoint( touch_postition );
+        world_pos.z = 0;
+
+        Rail new_rail = onLeftSide ? RightRail : LeftRail;
+        if( world_pos.y < new_rail.Bottom )
+            world_pos.y = new_rail.Bottom;
+        else if( world_pos.y > new_rail.Top )
+            world_pos.y = new_rail.Top;
+        world_pos.x = new_rail.RailCenter;
+
+        target_position = world_pos;
+        moving = true;
+        onLeftSide = !onLeftSide;
+    }
+}
