3606f09c948026de913bd8e3c47f0b8031684567
Skyler Hektner <skylerhektner@gmail.com>
2020-12-20 21:55:19 -0800

    partial spawning system setup

---
+++ Assets/Scripts/SpawnSystem/SpawnGroup.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CreateAssetMenu( fileName = "SpawnGroup", menuName = "ScriptableObjects/SpawnGroup", order = 1 )]
+[System.Serializable]
+public class SpawnGroup : ScriptableObject
+{
+    [SerializeField] public SpawnDictionary SpawnMap;
+    [SerializeField] public Layout layout;
+
+    [System.Serializable]
+    public enum EnemyEnum
+    {
+        Skeleton,
+        ShieldBearer,
+        ArcMage,
+        Shrike,
+        Shaman,
+    }
+
+    [System.Serializable]
+    public enum Layout
+    {
+        Cluster,
+        Spread,
+    }
+}
+
+[CustomEditor(typeof(SpawnGroup))]
+public class SpawnGroupEdtior : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        SpawnGroup spawn_group = (SpawnGroup)target;
+
+        if( spawn_group.SpawnMap == null  || spawn_group.SpawnMap.Count == 0)
+        {
+            spawn_group.SpawnMap = new SpawnDictionary();
+            foreach( var e in Enum.GetValues( typeof( SpawnGroup.EnemyEnum ) ) )
+                spawn_group.SpawnMap[(SpawnGroup.EnemyEnum)e] = 0;
+        }
+
+        foreach (var e in Enum.GetValues(typeof(SpawnGroup.EnemyEnum)))
+        {
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField( e.ToString() + ": " + spawn_group.SpawnMap[(SpawnGroup.EnemyEnum)e].ToString() );
+            if( GUILayout.Button( "+", GUILayout.Width( 25 ) ) )
+            {
+                spawn_group.SpawnMap[(SpawnGroup.EnemyEnum)e]++;
+                EditorUtility.SetDirty( target );
+            }
+            else if( GUILayout.Button( "-", GUILayout.Width( 25 ) ) && spawn_group.SpawnMap[(SpawnGroup.EnemyEnum)e] >= 1 )
+            {
+                spawn_group.SpawnMap[(SpawnGroup.EnemyEnum)e]--;
+                EditorUtility.SetDirty( target );
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        SpawnGroup.Layout layout = (SpawnGroup.Layout)EditorGUILayout.EnumPopup( "Layout: ", spawn_group.layout );
+        if( layout != spawn_group.layout)
+        {
+            spawn_group.layout = layout;
+            EditorUtility.SetDirty( target );
+        }
+    }
+}
+
+[Serializable] public class SpawnDictionary : SerializableDictionary<SpawnGroup.EnemyEnum, int> { }
+
+[Serializable]
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys = new List<TKey>();
+
+    [SerializeField]
+    private List<TValue> values = new List<TValue>();
+
+    // save the dictionary to lists
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        foreach( KeyValuePair<TKey, TValue> pair in this )
+        {
+            keys.Add( pair.Key );
+            values.Add( pair.Value );
+        }
+    }
+
+    // load dictionary from lists
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+
+        if( keys.Count != values.Count )
+            throw new System.Exception( string.Format( "there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable." ) );
+
+        for( int i = 0; i < keys.Count; i++ )
+            this.Add( keys[i], values[i] );
+    }
+}
