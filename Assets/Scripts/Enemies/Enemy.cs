7676ea9a231fd6de1c8a49bee98576a3b1920d97
Skyler Hektner <skylerhektner@gmail.com>
2020-12-23 22:47:06 -0800

    Fix sorting order so that sprites render on top of one another in an intuitive way

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -22,6 +23,7 @@ public class Enemy : MonoBehaviour
     public int CurrentHealth { get; private set; }
 
     protected Animator anim;
+    protected SpriteRenderer s_rend;
 
     //BaseHP PlayerBase;
 
@@ -31,6 +33,7 @@ public class Enemy : MonoBehaviour
         GameplayManager.TimeScaleChanged.AddListener( OnTimeScaleChange );
 
         anim = GetComponent<Animator>();
+        s_rend = GetComponent<SpriteRenderer>();
         if( SpawnEffect || ( SpawnAnimation != null && SpawnAnimation.Length > 0 ) )
             Spawn();
         else
@@ -59,6 +62,8 @@ public class Enemy : MonoBehaviour
             else
                 transform.position = transform.position + Vector3.down * MoveSpeed * Time.deltaTime * GameplayManager.GamePlayTimeScale;
         }
+
+        s_rend.sortingOrder = Convert.ToInt32( -transform.position.y * 100.0f );
     }
 
     public void Spawn()
