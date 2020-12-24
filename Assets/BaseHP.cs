5f64ac4c8650258bb88b8aeb58cb41a03ae67620
Amedes95 <pielord95@gmail.com>
2020-12-23 20:11:09 -0700

    BaseHP bar added
    Enemies do their remaining HP as damage to the base

---
+++ Assets/BaseHP.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseHP : MonoBehaviour
+{
+    public float CurrentHP;
+    public float MaxHP;
+
+    public HpBar CurrentHpBar;
+    public HpBar DamageHpBar;
+
+    float DamageDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHP = MaxHP;
+    }
+
+
+    public void ReduceHP(int Damage)
+    {
+        CurrentHP -= Damage;
+        CurrentHpBar.SetSize(CurrentHP / MaxHP);
+        DamageDelay = 1f;
+    }
+
+    private void FixedUpdate()
+    {
+        if (DamageDelay > 0)
+        {
+            DamageDelay -= Time.smoothDeltaTime;
+            if (DamageDelay <= 0)
+            {
+                DamageHpBar.SetSize(CurrentHP / MaxHP);
+            }
+        }
+    }
+
+
+
+}
