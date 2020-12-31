4c47435303d86021e326eb7f0b26fe1578127711
Amedes95 <pielord95@gmail.com>
2020-12-31 16:41:43 -0700

    Main menu/level select
    Rough draft, lookin spicy (:

---
+++ Assets/LoadLevel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+
+    public string SceneToLoad;
+    public bool locked;
+    public GameObject LockedSymbol;
+
+
+    public void Update()
+    {
+        if (locked)
+        {
+            LockedSymbol.SetActive(true);
+            gameObject.GetComponent<Button>().enabled = false;
+        }
+        else
+        {
+            LockedSymbol.SetActive(false);
+            gameObject.GetComponent<Button>().enabled = true;
+        }
+    }
+
+    public void LoadMyLevel()
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
---
+++ Assets/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject PlayCanvas;
+    public Animator Door;
+    public GameObject LevelCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        PlayCanvas.SetActive(false);
+        Door.SetTrigger("Open");
+    }
+
+    public void ShowLevels()
+    {
+        LevelCanvas.SetActive(true);
+    }
+}
