2c5a46a0f520c34a06b7bf0e28e640b27e72da46
Amedes95 <pielord95@gmail.com>
2021-01-10 15:56:16 -0700

    Pause screen second take

---
+++ Assets/PauseManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    public Animator WaveCounter;
+    public GameObject PauseScreen;
+
+    public void ResumeGame()
+    {
+        WaveCounter.SetBool("Visible", false);
+        Time.timeScale = 1;
+        PauseScreen.SetActive(false);
+    }
+
+
+    public void PauseGame()
+    {
+        PauseScreen.SetActive(true);
+        Time.timeScale = 0;
+        WaveCounter.SetBool("Visible", true);
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseGame();
+        }
+    }
+
+}
