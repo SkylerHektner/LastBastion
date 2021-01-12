65e244aa73fd62c84d4950cb8ef6fb303a4fdcca
Amedes95 <pielord95@gmail.com>
2021-01-11 23:33:10 -0700

    Menu touch up + victory screen
    Sweet sweet victory

---
+++ Assets/VictoryScreen.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VictoryScreen : MonoBehaviour
+{
+    public GameObject VictoryPopup;
+    public GameObject PauseCanvas;
+    public GameObject SawCanvas;
+    public GameObject AbilityManager;
+
+
+    [ContextMenu("DisplayVictory")]
+    public void DisplayVictory()
+    {
+        VictoryPopup.SetActive(true);
+        PauseCanvas.SetActive(false);
+        SawCanvas.SetActive(false);
+        AbilityManager.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
