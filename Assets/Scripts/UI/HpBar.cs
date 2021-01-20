48f2cfc2081fababdfd202f8dfbe2ee48c13c566
Amedes95 <pielord95@gmail.com>
2021-01-19 19:35:35 -0700

    Added FX for HP bar take damage
    Eh, first pass, not bad

--- Assets/Scripts/UI/HpBar.cs
+++ Assets/Scripts/UI/HpBar.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class HpBar : MonoBehaviour
 {
-
+    public Animator HurtFX;
     // scales healthbar
     public void SetSize(float sizeNormalized)
     {
         transform.localScale = new Vector3(sizeNormalized, 1f);
+        HurtFX.SetTrigger("Damaged");
     }
 
 }
