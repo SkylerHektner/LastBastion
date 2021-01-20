d03f0af531532d84cf6a6f8c1166279e3393a0c7
Skyler Hektner <skylerhektner@gmail.com>
2021-01-19 20:23:36 -0800

    Added time scale aware effect

---
+++ Assets/Scripts/Utility/TimeScaleAwareEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class TimeScaleAwareEffect : MonoBehaviour
+{
+    private Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        GameplayManager.Instance.TimeScaleChanged.AddListener( OnTimeScaleChanged );
+        anim.speed = GameplayManager.GamePlayTimeScale;
+    }
+
+    private void OnTimeScaleChanged()
+    {
+        anim.speed = GameplayManager.GamePlayTimeScale;
+    }
+
+    private void OnDestroy()
+    {
+        GameplayManager.Instance.TimeScaleChanged.RemoveListener( OnTimeScaleChanged );
+    }
+}
