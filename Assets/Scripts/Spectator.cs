7d092232ae2a93bc7d1b8c40ca72440f3ce67ed7
Amedes95 <pielord95@gmail.com>
2021-01-23 13:02:52 -0700

    LittleFixes

--- Assets/Scripts/Spectator.cs
+++ Assets/Scripts/Spectator.cs
@@ -40,20 +40,25 @@ public class Spectator : MonoBehaviour
 
     }
 
+    ////////// TOGGLE BACK ON LATER //////////////
     // game is quit mid-game, save my current progress and put me in limbo
-    private void OnApplicationQuit()
-    {
-        if (SceneManager.GetActiveScene().name != "Menu") // only save current progress if you are in a current level
-        {
-            PlayerPrefs.SetInt("LevelIndex", LevelIndex);
-            PlayerPrefs.SetInt("PlayerWealth", PlayerWealth);
-            PlayerPrefs.SetInt("Limbo", 1);
-            PlayerPrefs.SetString("ExitedScene", SceneManager.GetActiveScene().name);
-            // set current wave here too
-            // save base stats too
-            // save powerup charges
-        }
+    // this plays when you stop playing the game in-editor
 
-    }
+    //private void OnApplicationQuit()
+    //{
+    //    Debug.Log("donkeys");
+    //    if (SceneManager.GetActiveScene().name != "Menu") // only save current progress if you are in a current level
+    //    {
+    //        PlayerPrefs.SetInt("LevelIndex", LevelIndex);
+    //        PlayerPrefs.SetInt("PlayerWealth", PlayerWealth);
+    //        PlayerPrefs.SetInt("Limbo", 1);
+    //        PlayerPrefs.SetString("ExitedScene", SceneManager.GetActiveScene().name);
+    //        // set current wave here too
+    //        // save base stats too
+    //        // save powerup charges
+    //          // make sure player is not able to call this while dead
+    //    }
+
+    //}
 
 }
