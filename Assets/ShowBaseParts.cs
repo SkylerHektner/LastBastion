3396169a334de14776de6d66f08649876dad6dc8
Amedes95 <pielord95@gmail.com>
2021-02-15 20:58:57 -0700

    Tower parts toggle with unlocks now

---
+++ Assets/ShowBaseParts.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowBaseParts : MonoBehaviour
+{
+    public HpBar FuelBarR;
+    public HpBar FuelBarL;
+
+    public GameObject FuelBars;
+    public GameObject LightningNubs;
+    public GameObject AnomalyBars;
+    public GameObject SawMaggeddonConnectors;
+    public GameObject RecoverHPBar;
+    public GameObject SawmageddonBoxes;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        /// TYPHOON
+        if (PD.Instance.UpgradeUnlockMap.GetUnlock(PD.UpgradeFlags.Typhoon)) // Typhoon purchased? Show parts
+        {
+            FuelBars.SetActive(true);
+            if (TyphoonAbility.AnimatorDuration > 0)
+            {
+                FuelBarR.SetSize(TyphoonAbility.AnimatorDuration / 5); // replace 5 with fuel maximum amount
+                FuelBarL.SetSize(TyphoonAbility.AnimatorDuration / 5);
+            }
+        }
+        else
+        {
+            FuelBars.SetActive(false);
+        }
+
+        /// CHAIN LIGHTNING
+        if (PD.Instance.UpgradeUnlockMap.GetUnlock(PD.UpgradeFlags.ChainLightning))
+        {
+            LightningNubs.SetActive(true);
+        }
+        else
+        {
+            LightningNubs.SetActive(false);
+        }
+
+        /// TEMPORAL ANOMALY
+        if (PD.Instance.UpgradeUnlockMap.GetUnlock(PD.UpgradeFlags.Anomaly))
+        {
+            AnomalyBars.SetActive(true);
+        }
+        else
+        {
+            AnomalyBars.SetActive(false);
+        }
+
+        /// Sawmageddon HP Bar
+        if (PD.Instance.UpgradeUnlockMap.GetUnlock(PD.UpgradeFlags.Sawmageddon))
+        {
+            SawmageddonBoxes.SetActive(true);
+            if (SawmageddonAbility.AnimatorDuration > 0 && PD.Instance.UpgradeUnlockMap.GetUnlock(PD.UpgradeFlags.SawmageddonComboKiller)) // only show while active
+            {
+                SawMaggeddonConnectors.SetActive(true);
+                RecoverHPBar.SetActive(true);
+            }
+            else
+            {
+                SawMaggeddonConnectors.SetActive(false);
+                RecoverHPBar.SetActive(false);
+            }
+        }
+        else
+        {
+            SawmageddonBoxes.SetActive(false);
+        }
+    }
+
+
+
+}
