39b74d901dfac46d050c415ff630c14dbc649bf5
Skyler Hektner <skylerhektner@gmail.com>
2021-04-05 00:26:39 -0700

    Temporary fix for typhoon fuel bars extending past maximum

--- Assets/Scripts/UI/HpBar.cs
+++ Assets/Scripts/UI/HpBar.cs
@@ -10,13 +10,13 @@ public class HpBar : MonoBehaviour
     // scales healthbar
     public void SetSize( float sizeNormalized )
     {
-        transform.localScale = new Vector3( sizeNormalized, 1f );
+        transform.localScale = new Vector3( Mathf.Clamp( sizeNormalized, 0.0f, 1.0f ), 1f );
         if( HurtFX != null )
             HurtFX.SetTrigger( "Damaged" );
     }
     public void PlayHealAnim()
     {
-        HealFX.SetTrigger("Heal");
+        HealFX.SetTrigger( "Heal" );
 
     }
 
