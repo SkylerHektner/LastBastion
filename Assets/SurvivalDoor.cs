0b675e48ea440dffafdcc070d7bb8d4bca2a13aa
Amedes95 <Amedes95@gmail.com>
2021-05-03 00:22:44 -0600

    Survival menu part 3

--- Assets/SurvivalDoor.cs
+++ Assets/SurvivalDoor.cs
@@ -2,25 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class SurvivalDoor : MonoBehaviour
 {
     public Animator MyDoor;
-    public string HighestScoreText;
+    public TextMeshProUGUI HighestScore;
     //public GameObject ConfirmationMenu;
     public Button MyButton;
+    public string SceneToLoad;
+    public Image DoorPortal;
+    public Color GlowColor;
 
+    //public void Update()
+    //{
+    //        HighestScore.text == // highest score.text
+    //}
 
     public void OpenDoor()
     {
+        DoorPortal.color = GlowColor;
         MyDoor.SetTrigger("Open");
-
     }
 
     public void CloseDoor()
     {
+        DoorPortal.color = GlowColor;
         MyDoor.SetTrigger("Close");
-
     }
 
     public void AskConfirmation() // ask before loading level
@@ -28,13 +37,20 @@ public class SurvivalDoor : MonoBehaviour
         //ConfirmationMenu.SetActive(true);
     }
 
-    public void DenyConfirmation()
-    {
+    //public void DenyConfirmation()
+    //{
 
+    //}
+
+    //public void AcceptConfirmation()
+    //{
+
+    //}
+
+    // called by door animator
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(SceneToLoad);
     }
 
-    public void AcceptConfirmation()
-    {
-
-    }
 }
