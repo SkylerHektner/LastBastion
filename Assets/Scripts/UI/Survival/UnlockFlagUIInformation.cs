acae0f928ef256c5eca1aaba2112415de63df507
Amedes95 <Amedes95@gmail.com>
2021-06-24 23:37:44 -0600

    Added survival icons and glows

--- Assets/Scripts/UI/Survival/UnlockFlagUIInformation.cs
+++ Assets/Scripts/UI/Survival/UnlockFlagUIInformation.cs
@@ -13,4 +13,35 @@ public class UnlockFlagUIInformation : ScriptableObject
     public string Description;
     public int CampaignCost;
     public Sprite SurvivalIcon;
+
+    // colors for icon glow!
+    public bool Red;
+    public bool Green;
+    public bool Blue;
+    public bool Orange;
+    public bool Purple;
+    public bool Yellow;
+    public bool White;
+    public bool Opaque;
+
+    Vector4 RedColor = new Vector4(1, 0, 0, 1);
+    Vector4 GreenColor = new Vector4(0, 1, 0, 1);
+    Vector4 BlueColor = new Vector4(.25f, 0.7f, 1,1);
+    Vector4 PurpleColor = new Vector4(1, 0.3f, 1, 1);
+    Vector4 YellowColor = new Vector4(1, 1, 0, 1);
+    Vector4 OrangeColor = new Vector4(1, .7f, 0, 1);
+    Vector4 WhiteColor = new Vector4(1, 1, 1, 1);
+    Vector4 OpaqueColor = new Vector4(1, 1, 1, 0);
+
+    Color ActiveGlowRGB;
+
+    [ContextMenu("UpdateGlowColor")]
+    public void UpdateGlowColor()
+    {
+        if (Red)
+        {
+            ActiveGlowRGB = RedColor;
+        }
+    }
+
 }
