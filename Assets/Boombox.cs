3cd0f83570955d5f88c2427d81ec6de6e2c04269
Amedes95 <Amedes95@gmail.com>
2021-08-14 21:46:53 -0600

    Minor audio changes

--- Assets/Boombox.cs
+++ Assets/Boombox.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Boombox : MonoBehaviour
 {
     AudioSource CurrentSoundTrack;
+    public bool DeathMusic;
+    public bool VictoryMusic;
 
 
-    public void SwapTrack(AudioClip NextSong) // changes out the current music track
+    public void SwapTrack(AudioClip NextSong) // changes out the current music track in the spectator object
     {
         Spectator.GameMusic = gameObject.GetComponent<AudioSource>();
         Spectator.GameMusic.clip = NextSong;
@@ -17,6 +19,10 @@ public class Boombox : MonoBehaviour
 
     public void Awake() // when loading a level,find the level soundtrack and update the main boombox
     {
+        if (DeathMusic || VictoryMusic)
+        {
+            Spectator.GameMusic.clip = null;
+        }
         AudioSource LevelMusicPlayer = gameObject.GetComponent<AudioSource>();
         SwapTrack(LevelMusicPlayer.clip);
     }
