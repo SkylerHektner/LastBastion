4a6342c9da3c84a85631af518a59a354d1cb1b4b
Amedes95 <Amedes95@gmail.com>
2021-09-25 17:03:46 -0600

    Customization page second pass
    fuck scrollers and buttons

---
+++ Assets/CustomizationItem.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CustomizationItem : MonoBehaviour
+{
+
+    public GameObject MyChains;
+    public string TypeID;
+    public GameObject MyButton;
+    public Image ColoredBG;
+    public TextMeshProUGUI DescriptorText;
+    public Achievement AchievementInformation; // get rid of this later
+    public bool Purchased // toggles chains on/off if purchased or not
+    {
+        get
+        {
+            return ItemPurchased;
+        }
+        set
+        {
+            ItemPurchased = value;
+
+            if (ItemPurchased)
+            {
+                MyChains.SetActive(false);
+                MyButton.SetActive(true);
+                ColoredBG.color = Color.black;
+            }
+            else // not bought yet
+            {
+                MyChains.SetActive(true);
+                //MyButton.SetActive(false);
+                ColoredBG.color = Color.grey;
+            }
+        }
+    }
+    private bool ItemPurchased;
+    public bool CurrentlyEquipped
+    {
+        get
+        {
+            return Equipped;
+        }
+        set
+        {
+            Equipped = value;
+
+            if (Equipped)
+            {
+                ColoredBG.color = Color.green;
+            }
+            else
+            {
+                ColoredBG.color = Color.black;
+            }
+        }
+    }
+    private bool Equipped;
+
+
+
+
+    public void RattleChains() // toggles purchased status of item
+    {
+        if (Purchased == false)
+        {
+            MyChains.GetComponent<Animator>().SetTrigger("Rattle");
+        }
+        Debug.Log("Purchased: " + Purchased);
+    }
+    [ContextMenu("ToggleActive")]
+    public void ToggleActive() // toggles current active item
+    {
+        if (Purchased)
+        {
+            CurrentlyEquipped = !CurrentlyEquipped;
+        }
+        Debug.Log("Currently Active?: " + CurrentlyEquipped);
+    }
+
+    [ContextMenu("DebugPurchaseMe")]
+    public void DebugPurchaseMe()
+    {
+        Purchased = true;
+    }
+
+    public void UpdateDescriptorText()
+    {
+        DescriptorText.text = AchievementInformation.Description;
+    }
+
+    public void WipeDescriptorText()
+    {
+        DescriptorText.text = "";
+    }
+
+}
