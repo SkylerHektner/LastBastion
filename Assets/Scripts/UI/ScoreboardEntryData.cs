3443bf9ae44080068c2279e063c342c2e0fc1550
Amedes95 <Amedes95@gmail.com>
2025-07-29 00:19:07 -0600

    first pass at score screen ordering

---
+++ Assets/Scripts/UI/ScoreboardEntryData.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreboardEntryData : MonoBehaviour
+{
+    public TextMeshProUGUI PlayerNameText;
+    public TextMeshProUGUI PlayerWaveText;
+    public Image PlayerProfilePicture;
+    public int HighestWave;
+
+
+    public void SetMyInfo(string MyName, int MyScore, Sprite MyPicture)
+    {
+        PlayerNameText.text = MyName;
+        PlayerWaveText.text = "Wave " + MyScore.ToString();
+        PlayerProfilePicture.sprite = MyPicture;
+        HighestWave = MyScore;
+    }
+}
--- Assets/Scripts/UI/ScoresCanvasManager.cs
+++ Assets/Scripts/UI/ScoresCanvasManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using TMPro;
+using System.Linq;
 
 public class ScoresCanvasManager : MonoBehaviour
 {
@@ -10,9 +12,15 @@ public class ScoresCanvasManager : MonoBehaviour
     public float DistanceBetweenElements;
     public GameObject ArrowL;
     public GameObject ArrowR;
-    public RectTransform LevelBar;
+    public RectTransform LevelBar; // the entries are stored in this parent object as a list of children
     public int NumberofElements;
     public int CurrentIndex;
+
+    public GameObject ScoreEntryPrefab;
+    GameObject ScoreEntryClone;
+    public List<ScoreboardEntryData> ListOfEntries = new List<ScoreboardEntryData>();
+    public Sprite DefaultProfilePic;
+
 
     private void Awake()
     {
@@ -62,7 +70,31 @@ public class ScoresCanvasManager : MonoBehaviour
             ArrowL.SetActive(true);
             ArrowR.SetActive(true);
         }
-
     }
+
+
+    // used to populate scoreboard prefab entries
+    public void CreateScoreEntries(string PlayerNameString, int PlayerBestWaveScore, Sprite PlayerPicture)
+    {
+        ScoreEntryClone = Instantiate(ScoreEntryPrefab, transform.position, Quaternion.identity);
+        ScoreEntryClone.GetComponent<ScoreboardEntryData>().SetMyInfo(PlayerNameString, PlayerBestWaveScore, PlayerPicture);
+        ScoreEntryClone.transform.parent = LevelBar;
+        ScoreEntryClone.transform.localScale = new Vector3(1, 1, 1);
+        ScoreboardEntryData[] ChildrenEntries = GetComponentsInChildren<ScoreboardEntryData>();
+        ScoreboardEntryData[] ChildrenOrdered = ChildrenEntries.OrderBy(go => go.HighestWave).ToArray();
+        for (int i = 0; i < ChildrenOrdered.Length; i++)
+        {
+            ChildrenOrdered[i].transform.SetSiblingIndex(i);
+        }
+        NumberofElements = ChildrenEntries.Length;
+        ShowArrows();
+    }
+
+    [ContextMenu("TestEntries")]
+    public void TestEntries()
+    {
+        CreateScoreEntries("pussySlayerxx420", Random.Range(1, 200), DefaultProfilePic);
+    }
+   
 
 }
